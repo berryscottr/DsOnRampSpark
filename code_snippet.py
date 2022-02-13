984d300134116d9f8411d1c84f491c06dbc5d69d
Scott Berry <berryscottr@gmail.com>
2022-02-12 20:14:55 -0500

    ass 2 complete

--- code_snippet.py
+++ code_snippet.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
 
 if __name__ == '__main__':
     spark = (SparkSession
@@ -10,7 +9,7 @@ if __name__ == '__main__':
 
     text_file = sc.textFile("assignment_2_datafile.txt")
     counts = text_file.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
+        .map(lambda word: (word, 1) if len(word) > 2 else (word, 0)) \
         .reduceByKey(lambda x, y: x + y)
 
     output = counts.collect()
@@ -20,48 +19,3 @@ if __name__ == '__main__':
 
     sc.stop()
     spark.stop()
-
-
-
-    # df = spark.createDataFrame(rows)
-    # df.show()
-    #
-    # df2 = df.withColumn("text_left_over", f.expr("filter(text, x -> not(length(x) < 3))"))
-    # df2.show()
-    #
-    # # This is the actual piece of logic you are looking for.
-    # df3 = df.withColumn("text_left_over", f.expr("filter(text, x -> not(length(x) < 3))")).where(f.size(f.col("text_left_over")) > 0).drop("text")
-    # df3.show()from pyspark.sql import SparkSession
-    # from pyspark.sql import functions as f
-    #
-    # if __name__ == '__main__':
-    #     spark = (SparkSession
-    #              .builder
-    #              .appName("WordCount")
-    #              .getOrCreate())
-    #     sc = spark.sparkContext
-    #
-    #     text_file = sc.textFile("assignment_2_datafile.txt")
-    #     counts = text_file.flatMap(lambda line: line.split(" ")) \
-    #         .map(lambda word: (word, 1)) \
-    #         .reduceByKey(lambda x, y: x + y)
-    #
-    #     output = counts.collect()
-    #     for (word, count) in output:
-    #         if len(word) >= 3:
-    #             print("%s: %i" % (word, count))
-    #
-    #     sc.stop()
-    #     spark.stop()
-    #
-    #
-    #
-    #     # df = spark.createDataFrame(rows)
-    #     # df.show()
-    #     #
-    #     # df2 = df.withColumn("text_left_over", f.expr("filter(text, x -> not(length(x) < 3))"))
-    #     # df2.show()
-    #     #
-    #     # # This is the actual piece of logic you are looking for.
-    #     # df3 = df.withColumn("text_left_over", f.expr("filter(text, x -> not(length(x) < 3))")).where(f.size(f.col("text_left_over")) > 0).drop("text")
-    #     # df3.show()
