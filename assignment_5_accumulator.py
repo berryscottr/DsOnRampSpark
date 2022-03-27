3bea341dcb8bf6c346c40200ec58bcdf7f884a8d
Scott Berry <berryscottr@gmail.com>
2022-03-27 16:13:22 -0400

    ass 5 complete

---
+++ assignment_5_accumulator.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext
+
+if __name__ == '__main__':
+    sc = SparkContext("local", "Accumulator app")
+    num = sc.accumulator(10)
+
+
+    def f(x):
+        global num
+        num += x
+
+
+    rdd = sc.parallelize([20, 30, 40, 50])
+    rdd.foreach(f)
+    final = num.value
+    print("Accumulated value is -> %i" % final)
